e3eeafb12a0a38457497fe55f5a3f85a01a1471b
Django Goat <chango.goat@gmail.com>
2024-12-04 00:30:56 +0200

    day3 first star

---
+++ 2024/cobus/day3/day3.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	// "fmt"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func findInstructionPattern(data string) []string {
+	matchedPatternReg := []string{}
+	// fmt.Println(data)
+	for {
+		startPos := strings.Index(data, "mul(")
+		if startPos < 0 {
+			break
+		}
+		data = data[startPos+4:]
+
+		endPos := strings.Index(data, ")")
+		if endPos < 0 {
+			break
+		}
+		// fmt.Printf("startpos %d, endpos %d\n", startPos, endPos)
+		matchedPattern := data[:endPos]
+		// fmt.Println(matchedPattern)
+		matchedPatternReg = append(matchedPatternReg, matchedPattern)
+		data = data[endPos+1:]
+	}
+
+	return matchedPatternReg
+}
+
+func multiplyValidInstruction(dataEntry string) int {
+	entries := strings.Split(dataEntry, ",")
+
+	if len(entries) == 2 {
+		val1, err1 := strconv.Atoi(entries[0])
+		val2, err2 := strconv.Atoi(entries[1])
+
+		if err1 == nil && err2 == nil {
+			return val1 * val2
+		}
+	}
+	return -1
+}
+
+func AddValidInstructions(data string) int {
+	matchedPatternReg := findInstructionPattern(data)
+	tally := 0
+
+	for _, match := range matchedPatternReg {
+		val := multiplyValidInstruction(match)
+
+		if val == -1 {
+			// fmt.Printf("not valid instruction: %s\n", match)
+			lastTryIndex := strings.LastIndex(match, "mul(") + 4
+      if lastTryIndex < 4 {
+        continue
+      }
+			// fmt.Printf("last try val: %s, index: %d\n", match[lastTryIndex:], lastTryIndex)
+			val = multiplyValidInstruction(match[lastTryIndex:])
+			if val == -1 {
+				continue
+			}
+		}
+		tally += val
+	}
+	return tally
+}
+
+func main() {
+	lineReg := []string{}
+	tally := 0
+	file, _ := os.Open("./data")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineReg = append(lineReg, line)
+	}
+	for _, instr := range lineReg {
+		sum := AddValidInstructions(instr)
+		tally += sum
+	}
+	fmt.Println(tally)
+}
---
+++ 2024/cobus/day3/day3_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	// "fmt"
+	"reflect"
+	"testing"
+)
+
+func TestFindInstructions(t *testing.T) {
+	t.Run("find matched patterns of instructions", func(t *testing.T) {
+		got := findInstructionPattern(
+			"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
+		)
+		// fmt.Println(got)
+
+		want := []string{"2,4", "5,5", "32,64]then(mul(11,8", "8,5"}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+
+	t.Run("check for valid instructions", func(t *testing.T) {
+		got := multiplyValidInstruction("2,4")
+
+		want := 8
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+
+	t.Run("check for invalid instructions", func(t *testing.T) {
+		got := multiplyValidInstruction("32,64]then(mul(11,8")
+
+		want := -1
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+}
+
+func TestAddValidInstructions(t *testing.T) {
+	got := AddValidInstructions(
+		"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
+	)
+
+  want := 161
+
+  if got != want {
+    t.Errorf("got %d, want %d", got, want)
+  }
+}
