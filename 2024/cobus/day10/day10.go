81ad1ee65136252d6efb54cca5e71a666269e1b2
Cobus Linde <barendl@staycontactable.com>
2024-12-15 14:29:43 +0200

    day10 task1 done

--- 2024/cobus/day10/day10.go
+++ 2024/cobus/day10/day10.go
@@ -1,128 +1,86 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
-type Slope struct {
-	height   int
-	position [2]int
-}
+// import "fmt"
 
-type MapList struct {
-	startPosition [2]int
-	branches      []Slope
+func buildMap(trailMap []string) [][]int {
+	trail := [][]int{}
+
+	for _, line := range trailMap {
+		row := []int{}
+		for _, char := range line {
+			if char == '.' {
+				row = append(row, -1)
+			} else {
+				row = append(row, int(char-'0'))
+			}
+		}
+		trail = append(trail, row)
+	}
+
+	return trail
 }
 
 var PossibleDirections = [4][2]int{
 	{0, 1}, {1, 0}, {-1, 0}, {0, -1},
 }
 
-func (m *MapList) branchOff(pos [2]int, height int) {
-	for branchIndex, branch := range m.branches {
-		currentSlopeHeight := branch.height
-		if currentSlopeHeight == 9 {
+func chartPath(endMap map[[2]int]bool, height int, position [2]int, trail [][]int) {
+	rowLen := len(trail)
+	colLen := len(trail[0])
+	for _, dir := range PossibleDirections {
+		posx := position[0] + dir[0]
+		posy := position[1] + dir[1]
+
+		if posx < 0 || posx >= colLen || posy < 0 || posy >= rowLen {
 			continue
 		}
-		for _, dir := range PossibleDirections {
-			if branch.position[0]+dir[0] != pos[0] || branch.position[1]+dir[1] != pos[1] {
-				continue
-			}
-			if currentSlopeHeight + 1 != height {
-				continue
-			}
-			// new branch
-			if branch.height != currentSlopeHeight {
-				slope := Slope{height: height, position: pos}
-        fmt.Println("slope")
-        fmt.Println(slope.height)
-				m.branches = append(m.branches, slope)
-        fmt.Println(m.branches)
+
+		if trail[posy][posx] == height {
+			newPos := [2]int{posx, posy}
+			if height == 9 {
+				endMap[newPos] = true
 			} else {
-        fmt.Println("updating branch", height, pos)
-				m.branches[branchIndex].height = height
-				m.branches[branchIndex].position = pos
+				chartPath(endMap, height+1, newPos, trail)
 			}
 		}
 	}
-  // fmt.Println(m.branches)
 }
 
-func buildMap(trailMap []string) [][]int {
-  trail := [][]int{}
-
-  for _, line := range trailMap {
-    row := []int{}
-    for _, char := range line {
-      if char == '.' {
-        row = append(row, -1)
-      } else {
-        row = append(row, int(char - '0'))
-      }
-    }
-    trail = append(trail, row)
-  }
-
-  return trail
-}
-
-func findPath(trail [][]int) int {
-	endMap := map[[2]int]map[[2]int]bool{}
-	startMaps := []MapList{}
+func FindPath(trail [][]int) int {
 	tally := 0
-	for ypos, line := range trail {
-		for xpos, height := range line {
-			pos := [2]int{xpos, ypos}
+	for posy, line := range trail {
+		for posx, height := range line {
+			position := [2]int{posx, posy}
 			if height == 0 {
-        slope := Slope{height: height, position: pos}
-				mapList := MapList{branches: []Slope{slope}, startPosition: pos}
-				startMaps = append(startMaps, mapList)
-				continue
-			}
-			for _, startMap := range startMaps {
-				startMap.branchOff(pos, height)
-				if height == 9 {
-					if _, ok := endMap[pos]; !ok {
-						endMap[pos] = make(map[[2]int]bool)
-					}
-					if _, ok := endMap[pos][startMap.startPosition]; !ok {
-						tally++
-					} else {
-						endMap[pos][startMap.startPosition] = true
-					}
-				}
+				endMap := map[[2]int]bool{}
+				chartPath(endMap, height+1, position, trail)
+				// fmt.Println(endMap, len(endMap))
+				tally += len(endMap)
 			}
 		}
 	}
-	// for ypos := len(trail) - 1; ypos >= 0; ypos-- {
-	// 	for xpos, height := range trail[ypos] {
-	// 		pos := [2]int{xpos, ypos}
-	// 		if height == 0 {
-	// 			continue
-	// 		}
-	// 		for _, startMap := range startMaps {
-	// 			startMap.branchOff(pos, height)
-	// 			if height == 9 {
-	// 				if _, ok := endMap[pos]; !ok {
-	// 					endMap[pos] = make(map[[2]int]bool)
-	// 				}
-	// 				if _, ok := endMap[pos][startMap.startPosition]; !ok {
-	// 					tally++
-	// 				} else {
-	// 					endMap[pos][startMap.startPosition] = true
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// }
-  //
-	// for _, sm := range startMaps {
-	// 	fmt.Println(sm.startPosition)
-	// 	for _, branch := range sm.branches {
-	// 		fmt.Println(branch)
-	// 	}
-	// }
-	// for _, em := range endMap {
-	// 	fmt.Println(em)
-	// }
-  fmt.Println(startMaps)
 	return tally
+}
+
+func main() {
+	grid := []string{}
+
+	file, _ := os.Open("./data")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		grid = append(grid, line)
+	}
+  trail := buildMap(grid)
+
+  trailheads := FindPath(trail)
+  fmt.Println(trailheads)
 }
--- 2024/cobus/day10/day10_test.go
+++ 2024/cobus/day10/day10_test.go
@@ -10,8 +10,8 @@ func TestPathFinder(t *testing.T) {
 			"8765",
 			"9876",
 		}
-    trail := buildMap(trailString)
-		got := findPath(trail)
+		trail := buildMap(trailString)
+		got := FindPath(trail)
 
 		want := 1
 
@@ -20,6 +20,66 @@ func TestPathFinder(t *testing.T) {
 		}
 	})
 
+	t.Run("finds two paths", func(t *testing.T) {
+		trailString := []string{
+			"...0...",
+			"...1...",
+			"...2...",
+			"6543456",
+			"7.....7",
+			"8.....8",
+			"9.....9",
+		}
+		trail := buildMap(trailString)
+		got := FindPath(trail)
+
+		want := 2
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+
+	t.Run("finds four paths", func(t *testing.T) {
+		trailString := []string{
+			"..90..9",
+			"...1.98",
+			"...2..7",
+			"6543456",
+			"765.987",
+			"876....",
+			"987....",
+		}
+		trail := buildMap(trailString)
+		got := FindPath(trail)
+
+		want := 4
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+
+	t.Run("finds multiple paths", func(t *testing.T) {
+		trailString := []string{
+			"89010123",
+			"78121874",
+			"87430965",
+			"96549874",
+			"45678903",
+			"32019012",
+			"01329801",
+			"10456732",
+		}
+		trail := buildMap(trailString)
+		got := FindPath(trail)
+
+		want := 36 
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
 	// t.Run("find multiple paths", func(t *testing.T) {
 	// 	trail := [][]int{
 	// 		{8, 9, 0, 1, 0, 1, 2, 3},
