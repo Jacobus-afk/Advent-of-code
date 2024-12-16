81d53a0cbb7e58c82f76765680d61769b37fd3ef
Cobus Linde <barendl@staycontactable.com>
2024-12-17 00:29:57 +0200

    added go routinnes, still slow

--- 2024/cobus/day11/day11.go
+++ 2024/cobus/day11/day11.go
@@ -4,9 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 )
+
+var mapMutex sync.RWMutex
 
 func blinkChecks(
 	stone string,
@@ -16,27 +20,36 @@ func blinkChecks(
 	if stone == "0" {
 		return "1", ""
 	}
-	if stoneLen := len(stone); stoneLen%2 == 0 {
+	if stoneLen := uint64(len(stone)); stoneLen%2 == 0 {
+    mapMutex.RLock()
 		parts, ok := splitMemos[stone]
+    mapMutex.RUnlock()
 		if !ok {
 			halfStone := stoneLen / 2
 			part1 := stone[:halfStone]
 			part2 := stone[halfStone:]
-			number, _ := strconv.Atoi(part2)
-			part2 = strconv.Itoa(number)
+			number, _ := strconv.ParseUint(part2, 10, 64)
+			part2 = strconv.FormatUint(number, 10)
 			parts = [2]string{part1, part2}
+      mapMutex.Lock()
 			splitMemos[stone] = parts
+      mapMutex.Unlock()
 		}
 
 		return parts[0], parts[1]
+		// return part1, part2
 	}
 
+  mapMutex.RLock()
 	answer, ok := multMemos[stone]
+  mapMutex.RUnlock()
 	if !ok {
-		number, _ := strconv.Atoi(stone)
+		number, _ := strconv.ParseUint(stone, 10, 64)
 		multiplied := number * 2024
-		answer = strconv.Itoa(multiplied)
+		answer = strconv.FormatUint(multiplied, 10)
+    mapMutex.Lock()
 		multMemos[stone] = answer
+    mapMutex.Unlock()
 	}
 	return answer, ""
 }
@@ -47,47 +60,53 @@ func Blinks(blinkCount int, stones []string) []string {
 
 	stoneReg := []string{}
 
+	// resultsChannel := make(chan []string, len(stones))
+  resultsSlice := make([][]string, len(stones))
+
+	var wg sync.WaitGroup
+	// wg.Add(len(stones))
 	for pos := 0; pos < len(stones); pos++ {
-		tmpReg := []string{stones[pos]}
-		for count := 0; count < blinkCount; count++ {
-			fmt.Print(".")
-			for l := 0; l < len(tmpReg); l++ {
-				part1, part2 := blinkChecks(tmpReg[l], multMemos, splitMemos)
-				tmpReg[l] = part1
-				if part2 != "" {
-					tmpReg = append(tmpReg[:l+1], tmpReg[l:]...)
-					tmpReg[l+1] = part2
-					l++
+		tempReg := []string{stones[pos]}
+    wg.Add(1)
+		go func(tmpReg []string, id int) {
+      defer wg.Done()
+			// fmt.Printf("tmpReg %s\n", tmpReg)
+			for count := 0; count < blinkCount; count++ {
+				fmt.Print(id)
+				for l := 0; l < len(tmpReg); l++ {
+					part1, part2 := blinkChecks(tmpReg[l], multMemos, splitMemos)
+					// fmt.Println(part1, part2)
+					tmpReg[l] = part1
+					if part2 != "" {
+						tmpReg = append(tmpReg[:l+1], tmpReg[l:]...)
+						tmpReg[l+1] = part2
+						l++
+					}
+
 				}
-
 			}
-		}
+			fmt.Printf("id %d done\n", id)
+			// resultsChannel <- tmpReg
+      resultsSlice[id] = tmpReg
+		}(tempReg, pos)
 
-		fmt.Println(pos)
-		stoneReg = append(stoneReg, tmpReg...)
+		// fmt.Println(pos)
 	}
+	wg.Wait()
+  // fmt.Println("got here")
 
+	for i := 0; i < len(stones); i++ {
+		// fmt.Printf("result %d\n", i)
+		// r := <-resultsChannel
+    r := resultsSlice[i]
+		// fmt.Printf("result val: %s\n", r)
+		stoneReg = append(stoneReg, r...)
+	}
 	return stoneReg
-
-	// for t := 0; t < blinkCount; t++ {
-	// 	fmt.Println(t)
-	// 	for i := 0; i < len(stones); i++ {
-	// 		part1, part2 := blinkChecks(stones[i], multMemos, splitMemos)
-	// 		stones[i] = part1
-	// 		if part2 != "" {
-	// 			stones = append(stones[:i+1], stones[i:]...)
-	// 			stones[i+1] = part2
-	// 			i++
-	// 		}
-	// 		// fmt.Println(stones)
-	//
-	// 	}
-	// }
-	// // fmt.Println(memoizations)
-	// return stones
 }
 
 func main() {
+	fmt.Println(runtime.NumCPU())
 	stones := []string{}
 	file, _ := os.Open("./data")
 	defer file.Close()
