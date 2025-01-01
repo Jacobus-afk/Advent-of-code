f69c40eb1ed8bb025962563966effaee1ad61c97
Cobus Linde <barendl@staycontactable.com>
2025-01-01 20:44:23 +0200

    day15 task2 done

--- 2024/cobus/day15/day15.go
+++ 2024/cobus/day15/day15.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 
-  aoc "aoc-24/lib"
+	aoc "aoc-24/lib"
 )
 
 func initializeFromData(data []string) (map[[2]int]string, string, [2]int, [2]int) {
@@ -32,6 +32,44 @@ func initializeFromData(data []string) (map[[2]int]string, string, [2]int, [2]in
 	return warehouse, movements, robot, warehouseDimensions
 }
 
+func initializeAltWarehouse(data []string) (map[[2]int]string, string, [2]int, [2]int) {
+	handleMovements := false
+	warehouse := map[[2]int]string{}
+	robot := [2]int{-1, -1}
+	warehouseDimensions := [2]int{0, 0}
+	movements := ""
+	for posy, line := range data {
+		if handleMovements {
+			movements += line
+			continue
+		}
+		if line == "" {
+			handleMovements = true
+			warehouseDimensions = [2]int{len(data[posy-1]) * 2, posy}
+			continue
+		}
+		for idx, char := range line {
+			posx := idx * 2
+			switch char {
+			case '@':
+				robot = [2]int{posx, posy}
+				warehouse[[2]int{posx, posy}] = "@"
+				warehouse[[2]int{posx + 1, posy}] = "."
+			case 'O':
+				warehouse[[2]int{posx, posy}] = "["
+				warehouse[[2]int{posx + 1, posy}] = "]"
+			case '.':
+				warehouse[[2]int{posx, posy}] = "."
+				warehouse[[2]int{posx + 1, posy}] = "."
+			default:
+				warehouse[[2]int{posx, posy}] = "#"
+				warehouse[[2]int{posx + 1, posy}] = "#"
+			}
+		}
+	}
+	return warehouse, movements, robot, warehouseDimensions
+}
+
 func getMovementVector(movement rune) [2]int {
 	switch movement {
 	case '<':
@@ -44,6 +82,116 @@ func getMovementVector(movement rune) [2]int {
 		return [2]int{0, 1}
 	}
 	return [2]int{-99, -99}
+}
+
+func handleVerticalMovement(
+	robot, movementVector [2]int,
+	warehouse map[[2]int]string,
+	warehouseDimensions [2]int,
+) [2]int {
+	// fmt.Println("")
+	// for posy := range warehouseDimensions[1] {
+	// 	for posx := range warehouseDimensions[0] {
+	// 		fmt.Print(warehouse[[2]int{posx, posy}])
+	// 	}
+	// 	fmt.Println("")
+	// }
+	posx := robot[0]
+	posy := robot[1]
+	// potentialMove := warehouse[[2]int{posx, posy}]
+	// knockOnMoves := map[[2]int]string{{posx, posy}: potentialMove}
+	knockOnMoves := map[[2]int]string{robot: "@"}
+	movesList := []map[[2]int]string{}
+
+	for loop := true; loop; {
+		tmpMap := map[[2]int]string{}
+		// posx += movementVector[0]
+		// posy += movementVector[1]
+		// have to have loop false here so that we can break out if it's all '.'s
+		loop = false
+		for k, v := range knockOnMoves {
+			// fmt.Println("trying..", k, v)
+			posx = k[0] + movementVector[0]
+			posy = k[1] + movementVector[1]
+			// if warehouse[[2]int{posx, posy}] == "#" {
+			//   fmt.Println("got #", posx, posy, knockOnMoves)
+			//   return robot
+			// }
+			switch v {
+			case "@":
+				// posx = movementVector[0]
+				// posy = movementVector[1]
+				tmpMap[[2]int{posx, posy}] = warehouse[[2]int{posx, posy}]
+				// fmt.Println("got @ updated", tmpMap)
+				loop = true
+			case "[":
+				// posx += movementVector[0]
+				// posy += movementVector[1]
+				tmpMap[[2]int{posx, posy}] = warehouse[[2]int{posx, posy}]
+				tmpMap[[2]int{posx + 1, posy}] = warehouse[[2]int{posx + 1, posy}]
+				// fmt.Println("got [ updated", tmpMap)
+				loop = true
+			case "]":
+				// posx += movementVector[0]
+				// posy += movementVector[1]
+				tmpMap[[2]int{posx, posy}] = warehouse[[2]int{posx, posy}]
+				tmpMap[[2]int{posx - 1, posy}] = warehouse[[2]int{posx - 1, posy}]
+				// fmt.Println("got ] updated", tmpMap)
+				loop = true
+			case "#":
+				// fmt.Println("got #")
+				return robot
+			}
+		}
+		movesList = append(movesList, tmpMap)
+		knockOnMoves = tmpMap
+	}
+
+	for i := len(movesList) - 1; i >= 0; i-- {
+		moves := movesList[i]
+		// fmt.Println("rewinding..", moves)
+		for k := range moves {
+			posx = k[0] - movementVector[0]
+			posy = k[1] - movementVector[1]
+			replacement := warehouse[[2]int{posx, posy}]
+			// fmt.Printf(
+			// 	"replacing %s%d with %s[%d,%d]\n",
+			// 	warehouse[k],
+			// 	k,
+			// 	replacement,
+			// 	posx,
+			// 	posy,
+			// )
+			warehouse[k] = replacement
+			warehouse[[2]int{posx, posy}] = "."
+		}
+		// fmt.Println("")
+	}
+
+	posx = robot[0] + movementVector[0]
+	posy = robot[1] + movementVector[1]
+	warehouse[robot] = "."
+	robot = [2]int{posx, posy}
+	// warehouse[robot] = "@"
+
+	// for loop := true; loop; {
+	// 	potentialMove = warehouse[[2]int{posx, posy}]
+	// 	if posx < 0 || posy < 0 {
+	// 		break
+	// 	}
+	// 	switch potentialMove {
+	// 	case "[":
+	// 		posx += movementVector[0]
+	// 		posy += movementVector[1]
+	// 		posxAlt = posx + 1
+	// 	case "]":
+	// 		posx += movementVector[0]
+	// 		posy += movementVector[1]
+	// 		posxAlt = posx - 1
+	// 	}
+	// }
+
+	return robot
 }
 
 func handleMovement(robot, movementVector [2]int, warehouse map[[2]int]string) [2]int {
@@ -60,11 +208,14 @@ func handleMovement(robot, movementVector [2]int, warehouse map[[2]int]string) [
 		switch potentialMove {
 		case "#":
 			loop = false
-		case "O":
-			posx += movementVector[0]
-			posy += movementVector[1]
+		// case "O":
+		// 	posx += movementVector[0]
+		// 	posy += movementVector[1]
 		case ".":
 			loop = false
+		default:
+			posx += movementVector[0]
+			posy += movementVector[1]
 		}
 	}
 
@@ -92,6 +243,7 @@ func moveRobot(robot [2]int, movements string, warehouse map[[2]int]string) [2]i
 		movementVector := getMovementVector(movement)
 		// fmt.Println("movement", string(movement), movementVector)
 		robot = handleMovement(robot, movementVector, warehouse)
+
 		// fmt.Println(robot)
 	}
 
@@ -99,25 +251,73 @@ func moveRobot(robot [2]int, movements string, warehouse map[[2]int]string) [2]i
 	return robot
 }
 
-func CalcGPSCoordinates(data []string) int {
-  sumGPS := 0
+func moveAltRobot(
+	robot [2]int,
+	movements string,
+	warehouse map[[2]int]string,
+	warehouseDimensions [2]int,
+) [2]int {
+	for _, movement := range movements {
+		movementVector := getMovementVector(movement)
+		// fmt.Println("movement", string(movement), movementVector)
 
-  warehouse, movements, robot, _ := initializeFromData(data)
-  robot = moveRobot(robot, movements, warehouse)
+		// for posy := range warehouseDimensions[1] {
+		// 	for posx := range warehouseDimensions[0] {
+		// 		fmt.Print(warehouse[[2]int{posx, posy}])
+		// 	}
+		// 	fmt.Println("")
+		// }
 
-  for k, v := range warehouse {
-    if v == "O" {
-      gps := k[1] * 100 + k[0]
-      sumGPS += gps
-    }
-  }
+		if movementVector[1] == 0 {
+			robot = handleMovement(robot, movementVector, warehouse)
+		} else {
+			robot = handleVerticalMovement(robot, movementVector, warehouse, warehouseDimensions)
+		}
 
-  return sumGPS
+		// fmt.Println(robot)
+	}
+
+	fmt.Println("")
+	return robot
+}
+
+func CalcGPSCoordinates(data []string) (int, int) {
+	sumGPS := 0
+	sumAltGPS := 0
+
+	warehouse, movements, robot, _ := initializeFromData(data)
+	moveRobot(robot, movements, warehouse)
+
+	for k, v := range warehouse {
+		if v == "O" {
+			gps := k[1]*100 + k[0]
+			sumGPS += gps
+		}
+	}
+
+	warehouseAlt, movementsAlt, robotAlt, warehouseDimensions := initializeAltWarehouse(data)
+	moveAltRobot(robotAlt, movementsAlt, warehouseAlt, warehouseDimensions)
+
+	for posy := range warehouseDimensions[1] {
+		for posx := range warehouseDimensions[0] {
+			fmt.Print(warehouseAlt[[2]int{posx, posy}])
+		}
+		fmt.Println("")
+	}
+
+	for k, v := range warehouseAlt {
+		if v == "[" {
+			gps := k[1]*100 + k[0]
+			sumAltGPS += gps
+		}
+	}
+
+	return sumGPS, sumAltGPS
 }
 
 func main() {
-  data := aoc.ReadFileByLine("./data")
+	data := aoc.ReadFileByLine("./data")
 
-  sumGPS := CalcGPSCoordinates(data)
-  fmt.Println(sumGPS)
+	sumGPS, sumAltGPS := CalcGPSCoordinates(data)
+	fmt.Println(sumGPS, sumAltGPS)
 }
--- 2024/cobus/day15/day15_test.go
+++ 2024/cobus/day15/day15_test.go
@@ -1,102 +1,248 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"testing"
 )
 
-func TestInitialization(t *testing.T) {
-	t.Run("creates warehouse and robot movements", func(t *testing.T) {
-		data := []string{
-			"########",
-			"#..O.O.#",
-			"##@.O..#",
-			"#...O..#",
-			"#.#.O..#",
-			"#...O..#",
-			"#......#",
-			"########",
-			"",
-			"<^^>>>vv<v>>v<<",
-		}
-
-		warehouse, movements, robot, warehouseDimensions := initializeFromData(data)
-		fmt.Println(movements, robot)
-		fmt.Println("")
-		for posy := range warehouseDimensions[1] {
-			for posx := range warehouseDimensions[0] {
-				fmt.Print(warehouse[[2]int{posx, posy}])
-			}
-			fmt.Println("")
-		}
-	})
-
-	t.Run("creates warehouse and robot movements", func(t *testing.T) {
-		data := []string{
-			"##########",
-			"#..O..O.O#",
-			"#......O.#",
-			"#.OO..O.O#",
-			"#..O@..O.#",
-			"#O#..O...#",
-			"#O..O..O.#",
-			"#.OO.O.OO#",
-			"#....O...#",
-			"##########",
-			"",
-			"<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
-			"vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
-			"><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
-			"<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
-			"^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
-			"^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
-			">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
-			"<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
-			"^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
-			"v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
-		}
-
-		warehouse, movements, robot, warehouseDimensions := initializeFromData(data)
-		fmt.Println(movements, robot)
-		fmt.Println("")
-		for posy := range warehouseDimensions[1] {
-			for posx := range warehouseDimensions[0] {
-				fmt.Print(warehouse[[2]int{posx, posy}])
-			}
-			fmt.Println("")
-		}
-	})
-}
+// func TestInitialization(t *testing.T) {
+// 	t.Run("creates warehouse and robot movements", func(t *testing.T) {
+// 		data := []string{
+// 			"########",
+// 			"#..O.O.#",
+// 			"##@.O..#",
+// 			"#...O..#",
+// 			"#.#.O..#",
+// 			"#...O..#",
+// 			"#......#",
+// 			"########",
+// 			"",
+// 			"<^^>>>vv<v>>v<<",
+// 		}
+//
+// 		warehouse, movements, robot, warehouseDimensions := initializeFromData(data)
+// 		fmt.Println(movements, robot)
+// 		fmt.Println("")
+// 		for posy := range warehouseDimensions[1] {
+// 			for posx := range warehouseDimensions[0] {
+// 				fmt.Print(warehouse[[2]int{posx, posy}])
+// 			}
+// 			fmt.Println("")
+// 		}
+// 	})
+//
+// 	t.Run("creates warehouse and robot movements", func(t *testing.T) {
+// 		data := []string{
+// 			"##########",
+// 			"#..O..O.O#",
+// 			"#......O.#",
+// 			"#.OO..O.O#",
+// 			"#..O@..O.#",
+// 			"#O#..O...#",
+// 			"#O..O..O.#",
+// 			"#.OO.O.OO#",
+// 			"#....O...#",
+// 			"##########",
+// 			"",
+// 			"<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
+// 			"vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
+// 			"><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
+// 			"<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
+// 			"^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
+// 			"^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
+// 			">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
+// 			"<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
+// 			"^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
+// 			"v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
+// 		}
+//
+// 		warehouse, movements, robot, warehouseDimensions := initializeFromData(data)
+// 		fmt.Println(movements, robot)
+// 		fmt.Println("")
+// 		for posy := range warehouseDimensions[1] {
+// 			for posx := range warehouseDimensions[0] {
+// 				fmt.Print(warehouse[[2]int{posx, posy}])
+// 			}
+// 			fmt.Println("")
+// 		}
+// 	})
+//
+// 	t.Run("creates alt warehouse and robot movements", func(t *testing.T) {
+// 		data := []string{
+// 			"##########",
+// 			"#..O..O.O#",
+// 			"#......O.#",
+// 			"#.OO..O.O#",
+// 			"#..O@..O.#",
+// 			"#O#..O...#",
+// 			"#O..O..O.#",
+// 			"#.OO.O.OO#",
+// 			"#....O...#",
+// 			"##########",
+// 			"",
+// 			"<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
+// 			"vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
+// 			"><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
+// 			"<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
+// 			"^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
+// 			"^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
+// 			">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
+// 			"<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
+// 			"^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
+// 			"v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
+// 		}
+//
+// 		warehouse, movements, robot, warehouseDimensions := initializeAltWarehouse(data)
+// 		fmt.Println(movements, robot)
+// 		fmt.Println("")
+// 		for posy := range warehouseDimensions[1] {
+// 			for posx := range warehouseDimensions[0] {
+// 				fmt.Print(warehouse[[2]int{posx, posy}])
+// 			}
+// 			fmt.Println("")
+// 		}
+// 	})
+// }
 
 func TestRobotMovement(t *testing.T) {
-	t.Run("small warehouse robot movement", func(t *testing.T) {
-		data := []string{
-			"########",
-			"#..O.O.#",
-			"##@.O..#",
-			"#...O..#",
-			"#.#.O..#",
-			"#...O..#",
-			"#......#",
-			"########",
-			"",
-			"<^^>>>vv<v>>v<<",
-		}
+	// t.Run("small warehouse robot movement", func(t *testing.T) {
+	// 	data := []string{
+	// 		"########",
+	// 		"#..O.O.#",
+	// 		"##@.O..#",
+	// 		"#...O..#",
+	// 		"#.#.O..#",
+	// 		"#...O..#",
+	// 		"#......#",
+	// 		"########",
+	// 		"",
+	// 		"<^^>>>vv<v>>v<<",
+	// 	}
+	//
+	// 	warehouse, movements, robot, warehouseDimensions := initializeFromData(data)
+	//
+	// 	robot = moveRobot(robot, movements, warehouse)
+	//
+	// 	fmt.Println(movements, robot)
+	// 	for posy := range warehouseDimensions[1] {
+	// 		for posx := range warehouseDimensions[0] {
+	// 			fmt.Print(warehouse[[2]int{posx, posy}])
+	// 		}
+	// 		fmt.Println("")
+	// 	}
+	// })
+	//
+	// t.Run("large warehouse robot movement", func(t *testing.T) {
+	// 	data := []string{
+	// 		"##########",
+	// 		"#..O..O.O#",
+	// 		"#......O.#",
+	// 		"#.OO..O.O#",
+	// 		"#..O@..O.#",
+	// 		"#O#..O...#",
+	// 		"#O..O..O.#",
+	// 		"#.OO.O.OO#",
+	// 		"#....O...#",
+	// 		"##########",
+	// 		"",
+	// 		"<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
+	// 		"vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
+	// 		"><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
+	// 		"<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
+	// 		"^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
+	// 		"^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
+	// 		">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
+	// 		"<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
+	// 		"^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
+	// 		"v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
+	// 	}
+	//
+	// 	warehouse, movements, robot, warehouseDimensions := initializeFromData(data)
+	//
+	// 	robot = moveRobot(robot, movements, warehouse)
+	//
+	// 	fmt.Println(movements, robot)
+	// 	for posy := range warehouseDimensions[1] {
+	// 		for posx := range warehouseDimensions[0] {
+	// 			fmt.Print(warehouse[[2]int{posx, posy}])
+	// 		}
+	// 		fmt.Println("")
+	// 	}
+	// })
+	//
+	// t.Run("large alt warehouse robot movement", func(t *testing.T) {
+	// 	data := []string{
+	// 		"##########",
+	// 		"#..O..O.O#",
+	// 		"#......O.#",
+	// 		"#.OO..O.O#",
+	// 		"#..O@..O.#",
+	// 		"#O#..O...#",
+	// 		"#O..O..O.#",
+	// 		"#.OO.O.OO#",
+	// 		"#....O...#",
+	// 		"##########",
+	// 		"",
+	// 		"<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
+	// 		"vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
+	// 		"><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
+	// 		"<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
+	// 		"^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
+	// 		"^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
+	// 		">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
+	// 		"<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
+	// 		"^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
+	// 		"v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
+	// 	}
+	//
+	// 	warehouse, movements, robot, warehouseDimensions := initializeAltWarehouse(data)
+	//
+	// 	robot = moveRobot(robot, movements, warehouse)
+	//
+	// 	fmt.Println(movements, robot)
+	// 	for posy := range warehouseDimensions[1] {
+	// 		for posx := range warehouseDimensions[0] {
+	// 			fmt.Print(warehouse[[2]int{posx, posy}])
+	// 		}
+	// 		fmt.Println("")
+	// 	}
+	// })
+	//
+	// t.Run("calcsGPS coords", func(t *testing.T) {
+	// 	data := []string{
+	// 		"##########",
+	// 		"#..O..O.O#",
+	// 		"#......O.#",
+	// 		"#.OO..O.O#",
+	// 		"#..O@..O.#",
+	// 		"#O#..O...#",
+	// 		"#O..O..O.#",
+	// 		"#.OO.O.OO#",
+	// 		"#....O...#",
+	// 		"##########",
+	// 		"",
+	// 		"<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
+	// 		"vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
+	// 		"><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
+	// 		"<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
+	// 		"^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
+	// 		"^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
+	// 		">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
+	// 		"<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
+	// 		"^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
+	// 		"v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
+	// 	}
+	//
+	// 	get, _ := CalcGPSCoordinates(data)
+	//
+	// 	want := 10092
+	//
+	// 	if get != want {
+	// 		t.Errorf("get %d, want %d", get, want)
+	// 	}
+	// })
 
-		warehouse, movements, robot, warehouseDimensions := initializeFromData(data)
-
-		robot = moveRobot(robot, movements, warehouse)
-
-		fmt.Println(movements, robot)
-		for posy := range warehouseDimensions[1] {
-			for posx := range warehouseDimensions[0] {
-				fmt.Print(warehouse[[2]int{posx, posy}])
-			}
-			fmt.Println("")
-		}
-	})
-
-	t.Run("large warehouse robot movement", func(t *testing.T) {
+	t.Run("large alt warehouse gps calcs", func(t *testing.T) {
 		data := []string{
 			"##########",
 			"#..O..O.O#",
@@ -121,50 +267,34 @@ func TestRobotMovement(t *testing.T) {
 			"v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
 		}
 
-		warehouse, movements, robot, warehouseDimensions := initializeFromData(data)
+		_, get := CalcGPSCoordinates(data)
 
-		robot = moveRobot(robot, movements, warehouse)
+		want := 9021
 
-		fmt.Println(movements, robot)
-		for posy := range warehouseDimensions[1] {
-			for posx := range warehouseDimensions[0] {
-				fmt.Print(warehouse[[2]int{posx, posy}])
-			}
-			fmt.Println("")
+		if get != want {
+			t.Errorf("get %d, want %d", get, want)
 		}
 	})
 
-  t.Run("calcsGPS coords", func(t *testing.T) {
+	t.Run("small alt warehouse gps calcs", func(t *testing.T) {
 		data := []string{
-			"##########",
-			"#..O..O.O#",
-			"#......O.#",
-			"#.OO..O.O#",
-			"#..O@..O.#",
-			"#O#..O...#",
-			"#O..O..O.#",
-			"#.OO.O.OO#",
-			"#....O...#",
-			"##########",
+			"#######",
+			"#...#.#",
+			"#.....#",
+			"#..OO@#",
+			"#..O..#",
+			"#.....#",
+			"#######",
 			"",
-			"<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
-			"vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
-			"><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
-			"<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
-			"^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
-			"^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
-			">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
-			"<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
-			"^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
-			"v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
+			"<vv<<^^<<^^",
 		}
 
-    get := CalcGPSCoordinates(data)
+		_, get := CalcGPSCoordinates(data)
 
-    want := 10092
+		want := 618
 
-    if get != want {
-      t.Errorf("get %d, want %d", get, want)
-    }
-  })
+		if get != want {
+			t.Errorf("get %d, want %d", get, want)
+		}
+	})
 }
