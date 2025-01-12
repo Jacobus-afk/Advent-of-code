befb2453c04a497348dd464116c172b2c7144cc1
Django Goat <chango.goat@gmail.com>
2025-01-12 19:31:56 +0200

    some cleanup

--- 2024/cobus/day16/day16.go
+++ 2024/cobus/day16/day16.go
@@ -37,32 +37,11 @@ func tryDirection(
 	dimensions [2]int,
 	nodePoints map[[2]int]int,
 ) {
-	// updatedMap := map[[2]int]string{}
-	// for k, v := range maze {
-	// 	updatedMap[k] = v
-	// }
-	// possibleMoves := [][2]int{}
-	// localPoints := map[[2]int]bool{}
-	// for k,v := range dejavuPoints {
-	//   localPoints[k] = v
-	// }
 	for _, dir := range PossibleDirections {
 		localScore := score
 		xpos := dir[0] + currPos[0]
 		ypos := dir[1] + currPos[1]
 		newPos := [2]int{xpos, ypos}
-
-		// if point := localPoints[newPos]; point {
-		//   // discard, we're going in circles
-		//   continue
-		// }
-		//
-		// localPoints[newPos] = true
-
-		// if xpos == prevPos[0] && ypos == prevPos[1] {
-		// 	// discard, we're not moving back on path
-		// 	continue
-		// }
 
 		nextMazeBlock := maze[newPos]
 		// fmt.Println("current pos", currPos, "next block", nextMazeBlock, xpos, ypos, dir)
@@ -88,23 +67,9 @@ func tryDirection(
 		nodePoints[newPos] = localScore
 
 		if nextMazeBlock == "E" {
-			// pathScore[localScore] = true
-
-			// fmt.Println("")
-			// for posy := range dimensions[1] {
-			//   for posx := range dimensions[0] {
-			//     fmt.Print(maze[[2]int{posx, posy}])
-			//   }
-			//   fmt.Println("")
-			// }
-			// fmt.Println("")
-			//    fmt.Println(pathScore)
-
 			continue
 		}
-		// fmt.Println("updated score", currPos, localScore)
 
-		// updatedMap[currPos] = "X"
 		tryDirection(
 			maze,
 			newPos,
@@ -114,34 +79,16 @@ func tryDirection(
 			nodePoints,
 		)
 
-		// possibleMoves = append(possibleMoves, [2]int{xpos, ypos})
-
 	}
-	// fmt.Println("dead end")
-	// not needed i think
-	// if len(possibleMoves) == 0 {
-	// 	// this path is a dead end
-	// 	return
-	// }
 }
 
 func TraverseMaze(maze map[[2]int]string, start, end, dimensions [2]int) int {
 	nodePoints := map[[2]int]int{}
-	// pathScore := map[int]bool{}
-	// minScore := math.MaxInt64
 
 	tryDirection(maze, start, [2]int{1, 0}, 0, dimensions, nodePoints)
-	// fmt.Println(dejavuPoints[[2]int{1,13}], dejavuPoints[[2]int{1,9}], dejavuPoints[[2]int{3,9}], dejavuPoints[[2]int{3,7}], dejavuPoints[[2]int{11,7}])
 	fmt.Println(nodePoints)
-	// fmt.Println(pathScore)
 	fmt.Println(start)
 	fmt.Print("")
-
-	// for score := range pathScore {
-	// 	if minScore > score {
-	// 		minScore = score
-	// 	}
-	// }
 
 	return nodePoints[end]
 }
