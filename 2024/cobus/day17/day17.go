ba820a13573dbf677c807835b5c0eb9bad227f46
Django Goat <chango.goat@gmail.com>
2025-01-26 18:59:14 +0200

    task2 tests pass.. needs optimization for input

--- 2024/cobus/day17/day17.go
+++ 2024/cobus/day17/day17.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"math"
+	// "math"
 	"strconv"
 	"strings"
 
-  aoc "aoc-24/lib"
+	aoc "aoc-24/lib"
 )
 
 type Instruction struct {
@@ -23,96 +23,163 @@ type Computer struct {
 }
 
 func translateOperand(computer Computer, operand int) int {
-  switch operand {
-  case 0,1,2,3:
-    return operand
-  case 4:
-    return computer.registerA
-  case 5:
-    return computer.registerB
-  case 6:
-    return computer.registerC
-  case 7:
-    return -7
-  }
-  return -1
+	switch operand {
+	case 0, 1, 2, 3:
+		return operand
+	case 4:
+		return computer.registerA
+	case 5:
+		return computer.registerB
+	case 6:
+		return computer.registerC
+	case 7:
+		return -7
+	}
+	return -1
 }
 
 func adv(computer *Computer, operand int) {
-  combo := translateOperand(*computer, operand)
-  numerator := computer.registerA
-  denominator := math.Pow(2, float64(combo))
+	combo := translateOperand(*computer, operand)
+	numerator := computer.registerA
+	// denominator := math.Pow(2, float64(combo))
+	// denominator := 1 << combo
 
-  computer.registerA = numerator / int(denominator)
+	// computer.registerA = numerator / int(denominator)
+	computer.registerA = numerator >> combo
 }
 
 func bdv(computer *Computer, operand int) {
-  combo := translateOperand(*computer, operand)
-  numerator := computer.registerA
-  denominator := math.Pow(2, float64(combo))
+	combo := translateOperand(*computer, operand)
+	numerator := computer.registerA
+	// denominator := math.Pow(2, float64(combo))
+	// denominator := 1 << combo
 
-  computer.registerB = numerator / int(denominator)
+	// computer.registerB = numerator / int(denominator)
+	computer.registerB = numerator >> combo
 }
 
 func cdv(computer *Computer, operand int) {
-  combo := translateOperand(*computer, operand)
-  numerator := computer.registerA
-  denominator := math.Pow(2, float64(combo))
+	combo := translateOperand(*computer, operand)
+	numerator := computer.registerA
+	// denominator := math.Pow(2, float64(combo))
+	// denominator := 1 << combo
 
-  computer.registerC = numerator / int(denominator)
+	// computer.registerC = numerator / int(denominator)
+	computer.registerC = numerator >> combo
 }
 
 func bxl(computer *Computer, operand int) {
-  computer.registerB = computer.registerB ^ operand
+	computer.registerB = computer.registerB ^ operand
 }
 
 func bst(computer *Computer, operand int) {
-  combo := translateOperand(*computer, operand)
-  computer.registerB = combo % 8
+	combo := translateOperand(*computer, operand)
+	computer.registerB = combo & 7
 }
 
 func bxc(computer *Computer) {
-  computer.registerB = computer.registerB ^ computer.registerC
+	computer.registerB = computer.registerB ^ computer.registerC
 }
 
 func out(computer Computer, operand int) int {
-  combo := translateOperand(computer, operand)
-  return combo % 8
+	combo := translateOperand(computer, operand)
+	return combo & 7
 }
 
-func runInstructions(computer *Computer) []int {
-  output := []int{}
-  instrCtr := 0
+func handleInstruction(computer *Computer, instruction Instruction, output *[]int) int {
+	switch instruction.opcode {
+	case 0:
+		adv(computer, instruction.operand)
+	case 1:
+		bxl(computer, instruction.operand)
+	case 2:
+		bst(computer, instruction.operand)
+	case 3:
+		if computer.registerA != 0 {
+			// instrCtr = instruction.operand
+			// continue
+			return instruction.operand
+		}
+	case 4:
+		bxc(computer)
+	case 5:
+		outval := out(*computer, instruction.operand)
+		*output = append(*output, outval)
+	case 6:
+		bdv(computer, instruction.operand)
+	case 7:
+		cdv(computer, instruction.operand)
+	}
+	return -1
+}
 
-  for instrCtr < len(computer.program) * 2 {
-    instruction := computer.program[instrCtr]
+func RunInstructions(computer *Computer) []int {
+	output := []int{}
+	instrCtr := 0
 
-    switch instruction.opcode {
-    case 0:
-      adv(computer, instruction.operand)
-    case 1:
-      bxl(computer, instruction.operand)
-    case 2:
-      bst(computer, instruction.operand)
-    case 3:
-      if computer.registerA != 0 {
-        instrCtr = instruction.operand
-        continue
-      }
-    case 4:
-      bxc(computer)
-    case 5:
-      outval := out(*computer, instruction.operand)
-      output = append(output, outval)
-    case 6:
-      bdv(computer, instruction.operand)
-    case 7:
-      cdv(computer, instruction.operand)
-    }
+	for instrCtr < len(computer.program)*2 {
+		instruction := computer.program[instrCtr]
 
-    instrCtr += 2
-  }
-  return output
+		if resp := handleInstruction(computer, instruction, &output); resp != -1 {
+			instrCtr = resp
+			continue
+		}
+		instrCtr += 2
+	}
+	return output
+}
+
+func runToNextOutputInstruction(computer *Computer, outputInstruction int) bool {
+	output := []int{}
+	instrCtr := 0
+
+	for instrCtr < len(computer.program)*2 {
+		instruction := computer.program[instrCtr]
+
+		if resp := handleInstruction(computer, instruction, &output); resp != -1 {
+			if len(output) >= 2 {
+				break
+			}
+			instrCtr = resp
+			continue
+		}
+		instrCtr += 2
+	}
+
+	if len(output) == 2 && output[0] == computer.program[outputInstruction].opcode &&
+		output[1] == computer.program[outputInstruction].operand {
+    // fmt.Println("passed instruction", outputInstruction)
+		return true
+	}
+
+	return false
+}
+
+func FindProgramCopy(computer *Computer) int {
+	regACtr := 1
+	regB := computer.registerB
+	regC := computer.registerC
+	allOutputInstructionsValid := false
+	for {
+		computer.registerA = regACtr
+		computer.registerB = regB
+		computer.registerC = regC
+    // fmt.Println("trying computer", computer)
+
+		for i := 0; i < len(computer.program) * 2; i += 2 {
+			if validInstruction := runToNextOutputInstruction(computer, i); !validInstruction {
+				allOutputInstructionsValid = false
+				break
+			}
+			allOutputInstructionsValid = true
+		}
+		if allOutputInstructionsValid {
+			break
+		}
+		regACtr++
+	}
+
+  return regACtr
 }
 
 func initComputer(data []string) Computer {
@@ -133,16 +200,22 @@ func initComputer(data []string) Computer {
 		instrMap[i] = Instruction{opcode: opcode, operand: operand}
 	}
 
-  return Computer{registerA: regA, registerB: regB, registerC: regC, program: instrMap}
+	return Computer{registerA: regA, registerB: regB, registerC: regC, program: instrMap}
 }
 
 func main() {
-  data := aoc.ReadFileByLine("./data")
+	data := aoc.ReadFileByLine("./data")
 
-  computer := initComputer(data)
-  output := runInstructions(&computer)
-  for _, entry := range output {
-    fmt.Printf("%d,", entry)
-  }
+	computer := initComputer(data)
+  regB := computer.registerB
+  regC := computer.registerC
+	output := RunInstructions(&computer)
+	for _, entry := range output {
+		fmt.Printf("%d,", entry)
+	}
 
+  computer.registerB = regB
+  computer.registerC = regC
+  copyComp := FindProgramCopy(&computer)
+  fmt.Println(copyComp)
 }
--- 2024/cobus/day17/day17_test.go
+++ 2024/cobus/day17/day17_test.go
@@ -7,7 +7,6 @@ import (
 )
 
 func TestComputerInit(t *testing.T) {
-
 	t.Run("test bst", func(t *testing.T) {
 		data := []string{
 			"Register A: 0",
@@ -17,9 +16,9 @@ func TestComputerInit(t *testing.T) {
 			"Program: 2,6",
 		}
 
-    computer := initComputer(data)
-    runInstructions(&computer)
-    fmt.Println(computer)
+		computer := initComputer(data)
+		RunInstructions(&computer)
+		fmt.Println(computer)
 	})
 
 	t.Run("test out", func(t *testing.T) {
@@ -31,14 +30,14 @@ func TestComputerInit(t *testing.T) {
 			"Program: 5,0,5,1,5,4",
 		}
 
-    computer := initComputer(data)
-    got := runInstructions(&computer)
-    fmt.Println(computer)
-    want := []int{0,1,2}
+		computer := initComputer(data)
+		got := RunInstructions(&computer)
+		fmt.Println(computer)
+		want := []int{0, 1, 2}
 
-    if !reflect.DeepEqual(got, want) {
-      t.Errorf("got %d, want %d", got, want)
-    }
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d, want %d", got, want)
+		}
 	})
 
 	t.Run("test multiple", func(t *testing.T) {
@@ -50,14 +49,14 @@ func TestComputerInit(t *testing.T) {
 			"Program: 0,1,5,4,3,0",
 		}
 
-    computer := initComputer(data)
-    got := runInstructions(&computer)
-    fmt.Println(computer)
-    want := []int{4,2,5,6,7,7,7,7,3,1,0}
+		computer := initComputer(data)
+		got := RunInstructions(&computer)
+		fmt.Println(computer)
+		want := []int{4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0}
 
-    if !reflect.DeepEqual(got, want) {
-      t.Errorf("got %d, want %d", got, want)
-    }
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d, want %d", got, want)
+		}
 	})
 
 	t.Run("test bxl", func(t *testing.T) {
@@ -69,14 +68,14 @@ func TestComputerInit(t *testing.T) {
 			"Program: 1,7",
 		}
 
-    computer := initComputer(data)
-    runInstructions(&computer)
-    fmt.Println(computer)
-    // want := []int{4,2,5,6,7,7,7,7,3,1,0}
+		computer := initComputer(data)
+		RunInstructions(&computer)
+		fmt.Println(computer)
+		// want := []int{4,2,5,6,7,7,7,7,3,1,0}
 
-    // if !reflect.DeepEqual(got, want) {
-    //   t.Errorf("got %d, want %d", got, want)
-    // }
+		// if !reflect.DeepEqual(got, want) {
+		//   t.Errorf("got %d, want %d", got, want)
+		// }
 	})
 
 	t.Run("test bxc", func(t *testing.T) {
@@ -88,14 +87,14 @@ func TestComputerInit(t *testing.T) {
 			"Program: 4,0",
 		}
 
-    computer := initComputer(data)
-    runInstructions(&computer)
-    fmt.Println(computer)
-    // want := []int{4,2,5,6,7,7,7,7,3,1,0}
+		computer := initComputer(data)
+		RunInstructions(&computer)
+		fmt.Println(computer)
+		// want := []int{4,2,5,6,7,7,7,7,3,1,0}
 
-    // if !reflect.DeepEqual(got, want) {
-    //   t.Errorf("got %d, want %d", got, want)
-    // }
+		// if !reflect.DeepEqual(got, want) {
+		//   t.Errorf("got %d, want %d", got, want)
+		// }
 	})
 
 	t.Run("More complex ex", func(t *testing.T) {
@@ -107,35 +106,37 @@ func TestComputerInit(t *testing.T) {
 			"Program: 0,1,5,4,3,0",
 		}
 
-    computer := initComputer(data)
-    got := runInstructions(&computer)
+		computer := initComputer(data)
+		got := RunInstructions(&computer)
 
-    want := []int{4,6,3,5,6,3,5,2,1,0}
+		want := []int{4, 6, 3, 5, 6, 3, 5, 2, 1, 0}
 
-    if !reflect.DeepEqual(got, want) {
-      t.Errorf("got %d, want %d", got, want)
-    }
-    // fmt.Println(computer)
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d, want %d", got, want)
+		}
+		// fmt.Println(computer)
 	})
+}
 
+func TestFindProgramCopy(t *testing.T) {
 	t.Run("Trying to understand part2", func(t *testing.T) {
 		data := []string{
-			"Register A: 117440",
+			"Register A: 2024",
 			"Register B: 0",
 			"Register C: 0",
 			"",
 			"Program: 0,3,5,4,3,0",
 		}
 
-    computer := initComputer(data)
-    output := runInstructions(&computer)
+		computer := initComputer(data)
+		// output := RunInstructions(&computer)
+    got := FindProgramCopy(&computer)
+    want := 117440
 
-    // want := []int{4,6,3,5,6,3,5,2,1,0}
+    if got != want {
+      t.Errorf("got %d, want %d", got, want)
+    }
 
-    // if !reflect.DeepEqual(got, want) {
-    //   t.Errorf("got %d, want %d", got, want)
-    // }
-    fmt.Println(computer, output)
+		fmt.Println(computer)
 	})
-
 }
